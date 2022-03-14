ee1c678331c1fd06e85279bf5c34f043b858a437
Matdrox <Matdrox@users.noreply.github.com>
2022-03-14 16:31:38 +0100

    Updated DLL

--- 3-doubly-linked-list.py
+++ 3-doubly-linked-list.py
@@ -1,12 +1,35 @@
 class Node:
   def __init__(self, value):
     self.value = value
-    self.next, self.prev = None
+    self.next = None
+    self.prev = None
 
 class DoublyLinkedList:
   def __init__(self, value):
     new_node = Node(value)
-    self.head, self.tail = new_node
+    self.head = new_node
+    self.tail = new_node
     self.length = 1
 
-my_doubly_linked_list = DoublyLinkedList(7)
+  def print_list(self):
+    temp = self.head
+    while temp != None:
+      print(temp.value)
+      temp = temp.next
+
+  def append(self, value):
+    new_node = Node(value)
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.next = new_node
+      new_node.prev = self.tail
+      self.tail = new_node
+    self.length += 1
+    return True
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.print_list()
