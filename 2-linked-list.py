025a1596b8da273c78f18b7a578dbf8295bf5a26
Matdrox <Matdrox@users.noreply.github.com>
2022-03-31 19:43:23 +0200

    Finished Linked Lists

--- 2-linked-list.py
+++ 2-linked-list.py
@@ -1,15 +1,15 @@
 head = {
-  "value": 11,
-  "next": {
-    "value": 3,
+    "value": 11,
     "next": {
-      "value": 23,
-      "next": {
-        "value": 7,
-        "next": None
-      }
+        "value": 3,
+        "next": {
+            "value": 23,
+            "next": {
+                "value": 7,
+                "next": None
+            }
+        }
     }
-  }
 }
 
 # print(head['next']['next']['value'])
@@ -19,127 +19,129 @@ head = {
 
 
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 
 class LinkedList:
-  def __init__(self, value):
-    new_node = Node(value)
-    self.head = new_node
-    self.tail = new_node
-    self.length = 1
-  
-  def print_list(self):
-    temp = self.head
-    while temp is not None:
-      print(temp.value)
-      temp = temp.next
-  
-  def append(self, value):
-    new_node = Node(value)
-    if self.length == 0:             #Check if LL is empty
-      self.head = new_node
-      self.tail = new_node
-    else:
-      self.tail.next = new_node
-      self.tail = new_node
-    self.length += 1
-    return True
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
-  def pop(self):
-    if self.length == 0:
-      return None
-    pre = self.head
-    temp = self.head
-    while temp.next:
-      pre = temp
-      temp = temp.next
-    self.tail = pre
-    self.tail.next = None
-    self.length -= 1
-    if self.length == 0:
-      self.head = None
-      self.tail = None  
-    return temp
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-  def prepend(self, value):
-    new_node = Node(value)
-    if self.length == 0:
-      self.head = new_node
-      self.tail = new_node
-    else:
-      new_node.next = self.head
-      self.head = new_node
-    self.length += 1
-    return True
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:  # Check if LL is empty
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
-  def pop_first(self):
-    temp = self.head
-    if self.length == 0:
-      return None
-    else:
-      self.head = self.head.next
-      temp.next = None
-      self.length -= 1
-      if self.length == 0:
-        self.tail = None
-      return temp
-      
-  def get_value(self, index):
-    if index < 0 or index >= self.length:
-      return None
-    temp = self.head
-    for _ in range(index):              #No variable "i" for it is not used
-      temp = temp.next
-    return temp
-  
-  def set_value(self, index, value):
-    temp = self.get_value(index)
-    if temp:
-      temp.value = value
-      return True
-    return False
+    def pop(self):
+        if self.length == 0:
+            return None
+        pre = self.head
+        temp = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
-  def insert(self, index, value):
-    if index < 0 or index >= self.length:
-      return False
-    if index == 0:
-      return self.prepend(value)
-    if index == self.length:
-      return self.append(value)
-    new_node = Node(value)
-    temp = self.get_value(index-1)
-    new_node.next = temp.next
-    temp.next = new_node
-    self.length += 1
-    return True
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
-  def remove(self, index):
-    if index < 0 or index >= self.length:
-      return None
-    if index == 0:
-      return self.pop_first()
-    if index == self.length - 1:
-      return self.pop()
-    prev = self.get_value(index-1)
-    temp = prev.next
-    prev.next = temp.next     # Skip over temp in the list
-    temp.next = None          # Remove from list
-    self.length -= 1
-    return temp
+    def pop_first(self):
+        temp = self.head
+        if self.length == 0:
+            return None
+        else:
+            self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.tail = None
+            return temp
 
-  def revert(self):      # Interview Question
-    temp = self.head
-    self.head = self.tail
-    self.tail = temp
-    before = None
-    after = temp.next
-    for _ in range(self.length):
-      after = temp.next
-      temp.next = before
-      before = temp
-      temp = after
+    def get_value(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):  # No variable "i" for it is not used
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get_value(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get_value(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get_value(index-1)
+        temp = prev.next
+        prev.next = temp.next     # Skip over temp in the list
+        temp.next = None          # Remove from list
+        self.length -= 1
+        return temp
+
+    def revert(self):      # Interview Question
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
 
 my_linked_list = LinkedList(11)
 my_linked_list.append(3)
@@ -154,7 +156,7 @@ print(f'\n{my_linked_list.revert()}\n')
 my_linked_list.print_list()
 
 # print(my_linked_list.pop())
-#my_linked_list.prepend(1)
+# my_linked_list.prepend(1)
 # my_linked_list.pop_first()
 # print(my_linked_list.get_value(2))
 # my_linked_list.set_value(1, 4)
