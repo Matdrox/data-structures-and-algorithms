93dd71352a19fb3fa2b10050361d3b280a18e428
Matdrox <Matdrox@users.noreply.github.com>
2022-03-31 19:44:00 +0200

    Finished Doubly Linked Lists

--- 3-doubly-linked-list.py
+++ 3-doubly-linked-list.py
@@ -1,125 +1,128 @@
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
-    self.prev = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
 
 class DoublyLinkedList:
-  def __init__(self, value):
-    new_node = Node(value)
-    self.head = new_node
-    self.tail = new_node
-    self.length = 1
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
-  def print_list(self):
-    temp = self.head
-    while temp != None:
-      print(temp.value)
-      temp = temp.next
+    def print_list(self):
+        temp = self.head
+        while temp != None:
+            print(temp.value)
+            temp = temp.next
 
-  def append(self, value):
-    new_node = Node(value)
-    if self.head == None:
-      self.head = new_node
-      self.tail = new_node
-    else:
-      self.tail.next = new_node
-      new_node.prev = self.tail
-      self.tail = new_node
-    self.length += 1
-    return True
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
 
-  def pop(self):
-    if self.length == 0:
-      return None
-    if self.length == 1:
-      self.head = None
-      self.tail = None
-    else:
-      temp = self.tail
-      self.tail = self.tail.prev
-      self.tail.next = None
-      temp.prev.next = None
-    self.length -= 1
-    return temp
+    def pop(self):
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev.next = None
+        self.length -= 1
+        return temp
 
-  def prepend(self, value):
-    new_node = Node(value)
-    if self.length == 0:
-      self.head == new_node
-      self.tail == new_node
-    else:
-      new_node.next = self.head
-      self.head.prev = new_node
-      self.head = new_node
-    self.length += 1
-    return True
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head == new_node
+            self.tail == new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
 
-  def pop_first(self):
-    if self.length == 0:
-      return None
-    if self.length == 1:
-      self.head = None
-      self.tail = None
-    else:
-      temp = self.head
-      self.head = self.head.next
-      self.head.prev = None
-      temp.next = None
-    self.length -= 1
-    return temp
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
 
-  def get_value(self, index):
-    if index < 0 or index >= self.length:
-      return None
-    if index < self.length/2:     # Search in first half, starting from head
-      temp = self.head
-      for _ in range(index):
-        temp = temp.next
-    else:                         # Search in second half, starting from tail
-      temp = self.tail
-      for _ in range(self.length - 1, index, -1):     # Start from last index to index, decrement by 1
-        temp = temp.prev
-    return temp
+    def get_value(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index < self.length/2:     # Search in first half, starting from head
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+        else:                         # Search in second half, starting from tail
+            temp = self.tail
+            # Start from last index to index, decrement by 1
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
 
-  def set_value(self, index, value):
-    temp = self.get(index)
-    if temp:
-      temp.value = value
-      return True
-    return False
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
 
-  def insert(self, index, value):
-    if index < 0 or index > self.length:
-      return False
-    if index == 0:
-      return self.prepend(value)
-    if index == self.length:
-      return self.append(value)
-    new_node = Node(value)
-    before = self.get_value(index-1)
-    after = before.next
-    before.next = new_node
-    after.prev = new_node
-    new_node.prev = before
-    new_node.next = after
-    self.length += 1
-    return True
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get_value(index-1)
+        after = before.next
+        before.next = new_node
+        after.prev = new_node
+        new_node.prev = before
+        new_node.next = after
+        self.length += 1
+        return True
 
-  def remove(self, index):
-    if index == 0:
-      return self.pop_first()
-    if index == self.length:
-      return self.pop()
-    temp = self.get_value(index)
-    before = temp.prev
-    after = temp.next
-    temp.prev = None
-    temp.next = None
-    before.next = after
-    after.prev = before
-    self.length -= 1
-    return True
+    def remove(self, index):
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        temp = self.get_value(index)
+        before = temp.prev
+        after = temp.next
+        temp.prev = None
+        temp.next = None
+        before.next = after
+        after.prev = before
+        self.length -= 1
+        return True
+
 
 my_doubly_linked_list = DoublyLinkedList(0)
 my_doubly_linked_list.append(1)
